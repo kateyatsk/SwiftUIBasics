d597cf2f1156d1f2001ae0af58962fa934188b47
kateyatsk <klaettoe2023@gmail.com>
2025-04-16 21:48:07 +0300

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Екатерина Яцкевич on 16.04.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
